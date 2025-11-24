5a111a5b03916ff14ce81d269adbed39cdfde7d4
Sayantan Biswas <sayantanbiswas.mycareer@gmail.com>
2025-11-25 04:53:28 +0530

    Initialize project with Maven, Docker Compose, Spring Boot skeleton, and deployment scripts.

---
+++ src/test/java/schoolconnect/dev/DevApplicationTests.java
@@ -0,0 +1,13 @@
+package schoolconnect.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
